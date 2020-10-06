ba38c964ad4cb622a6ab9e5cdbc6f4be3657da50
Jimmy Ngo <jngo2013@gmail.com>
2020-10-06 16:18:19 -0700

    create Navbar

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import BootstrapCard from '../../components/BootstrapCard';
-
+import Navbar from './../../components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
         <div> Hello </div>
+        <Navbar></Navbar>
         <BootstrapCard />
       </div>  
+     
+          
+  
+      
     )
   }
 }
