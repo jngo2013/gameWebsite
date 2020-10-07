80fce852cdcc244a48304e1b4444deddace2f738
Jimmy Ngo <jngo2013@gmail.com>
2020-10-07 12:20:15 -0700

    center aligned text for HeadLine

--- client/src/components/HeadLine/index.js
+++ client/src/components/HeadLine/index.js
@@ -4,12 +4,14 @@ import "./style.css"
 
 export default (props) => (
   <div>
-<Jumbotron style={{ height: '30rem'}}>
+<Jumbotron className='jumbotron'>
+  <div>
   <h1 className="jumbotronText">Our Story</h1>
   <p className="jumbotronText">
     This is a simple hero unit, a simple jumbotron-style component for calling
     extra attention to featured content or information.
   </p>
+  </div>
 </Jumbotron>
 </div>
 );
