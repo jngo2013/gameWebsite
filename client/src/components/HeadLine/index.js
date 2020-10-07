eb4ee51be0b763526926ce5412791372c6282ca4
Jimmy Ngo <jngo2013@gmail.com>
2020-10-07 11:55:03 -0700

    expand height of HeadLine

--- client/src/components/HeadLine/index.js
+++ client/src/components/HeadLine/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 
 export default (props) => (
   <div>
-<Jumbotron>
+<Jumbotron style={{ height: '30rem'}}>
   <h1 className="jumbotronText">Our Story</h1>
   <p className="jumbotronText">
     This is a simple hero unit, a simple jumbotron-style component for calling
