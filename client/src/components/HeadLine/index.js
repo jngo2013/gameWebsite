cc196d4ad1d51d93158212c6cfe290fdcf4aa8d7
Jimmy Ngo <jngo2013@gmail.com>
2020-10-06 17:06:10 -0700

    fix Jumbotron

---
+++ client/src/components/HeadLine/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Jumbotron} from 'react-bootstrap';
+
+export default (props) => (
+  <div>
+<Jumbotron>
+  <h1 className="jumbotronText">Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+</Jumbotron>
+</div>
+);
--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import CardSection from './../CardSection';
 import Navbar from './../../components/Navbar'
+import HeadLine from './../../components/HeadLine'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div> Hello </div>
         <Navbar/>
+        <HeadLine/>
         <CardSection />
       </div>  
     )
