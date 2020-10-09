b6a37106b8c31c88c9148e0364deb4568dc9d54e
gcmartin <gscalica@gmail.com>
2020-10-09 11:13:56 -0700

    Add links to social media, add logo and style

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,7 +10,10 @@ function Footer() {
       <Row>
         <Col lg={8} md={12} className="Footer-col">
           <div className="Footer-col-div">
-            Logo
+            <img 
+              src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-0/p370x247/67529268_665627333913015_2337246147929702400_n.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=kSPsdRngwLcAX9UJVsW&_nc_ht=scontent-sjc3-1.xx&tp=6&oh=af476855dd55ba3c2227e2b324cb64cc&oe=5FA54906"
+              className="Footer-icon" 
+            />
           </div>
         </Col>
 
@@ -19,9 +22,9 @@ function Footer() {
           <div className="Footer-col-div">
             <h4>Social Media</h4>
             <ul>
-              <li>Facebook</li>
-              <li>Instagram</li>
-              <li>Youtube</li>
+              <li><a href="https://www.facebook.com/DrunkOnGamesChannel" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+              <li><a href="https://www.youtube.com/channel/UCUgcXszSeKLcAS6rT2h0Fjg" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+              <li><a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a></li>
             </ul>
           </div>
 
@@ -33,9 +36,9 @@ function Footer() {
           <div className="Footer-col-div">
             <h4>Contact</h4>
             <ul>
-              <li>Something</li>
-              <li>Something</li>
-              <li>Something</li>
+              <li>someone@gmail.com</li>
+              <li>someone@gmail.com</li>
+              <li>someone@gmail.com</li>
             </ul>
           </div>
 
