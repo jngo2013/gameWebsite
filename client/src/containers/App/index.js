362efb12bacde5d6d51183c084f18eac07752d06
Jimmy Ngo <jngo2013@gmail.com>
2020-10-13 15:54:17 -0700

    install React Router

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import CardSection from './../CardSection';
 import Navbar from './../../components/Navbar'
+import { Route } from "react-router-dom";
 import ControlledCarousel from './../../components/EventCarousel'
 import Footer from './../../components/Footer';
-
+import Container from 'react-bootstrap/Container'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Container fluid>
         <Navbar/>
+        </Container>
+
+        <div>Title</div>
+        <Container fluid>
         <ControlledCarousel/>
         <CardSection />
         <Footer />
+        </Container>
       </div>  
     )
   }
