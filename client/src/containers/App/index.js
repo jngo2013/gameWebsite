d4b98841258f5e4afb9ba68dcb2de0a71c410e16
Jimmy Ngo <jngo2013@gmail.com>
2020-10-13 16:20:12 -0700

    fixed Router

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import CardSection from './../CardSection';
 import Navbar from './../../components/Navbar'
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router,Route } from "react-router-dom";
 import ControlledCarousel from './../../components/EventCarousel'
 import Footer from './../../components/Footer';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+
+import AboutUs from '../AboutUs'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Container fluid>
-        <Navbar/>
-        </Container>
-
+          <Router>
+          <Navbar/> 
+        <Route exact path="/AboutUs" component={AboutUs}/>
         <div>Title</div>
-        <Container fluid>
         <ControlledCarousel/>
         <CardSection />
         <Footer />
+          </Router>
         </Container>
       </div>  
     )
