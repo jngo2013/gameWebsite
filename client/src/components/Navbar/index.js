655d31060fade23251291a7172bd89d3248323e7
Jimmy Ngo <jngo2013@gmail.com>
2020-10-23 15:10:59 -0700

    fix home icon in navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { NavDropdown, Nav, Form, Navbar, Button, FormControl } from 'react-bootstrap';
+import { NavDropdown, Nav, Form, Navbar, Image } from 'react-bootstrap';
 import "./style.css";
 import SearchBar from "../SearchBar/index"
+import sample2 from "./images/sample2.jpg"
 
 export default (props) => (
 <Navbar fixed='top'  className="NavbarBG" expand="lg">
   <Navbar.Toggle  aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="AboutUs">About Us</Nav.Link>
-      <NavDropdown title="Games" id="basic-nav-dropdown">
+      <Nav.Link href="/"><Image   src={sample2} className="navbar-brand" roundedCircle></Image></Nav.Link>
+      <Nav.Link href="AboutUs" className="navPadding" >About Us</Nav.Link>
+      <NavDropdown title="Games" id="basic-nav-dropdown"  className="navPadding">
         <NavDropdown.Item href="/LegacyGames">Legacy</NavDropdown.Item>
         <NavDropdown.Item href="#Events">Events</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Hooplah</NavDropdown.Item>
@@ -20,7 +21,6 @@ export default (props) => (
     </Nav>
     <Form inline>
       <SearchBar />
-      
     </Form>
   </Navbar.Collapse>
 </Navbar>
