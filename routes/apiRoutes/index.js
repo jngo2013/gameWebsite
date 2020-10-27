9d086a1b38308a57954cb3893a9dc67cb4adc78d
gcmartin <gscalica@gmail.com>
2020-10-27 11:13:22 -0700

    Create 'controllers' folder, legacyGames API routes, and require routes in server file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const legacyGames = require('./legacyGames');
 
-
+// "/api" prepended here
+// legacy games route
+router.use("/legacygames", legacyGames);
 
 module.exports = router;
