fe32647cf8a018b64aee906f351fe1d44109fe4c
Jimmy Ngo <jngo2013@gmail.com>
2020-10-30 11:44:47 -0700

    add authentication middleware

---
+++ middlewares/index.js
@@ -0,0 +1,10 @@
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+const requireSignIn = passport.authenticate('local', { session: false });
+
+module.exports = {
+  requireAuth,
+  requireSignIn,
+};
