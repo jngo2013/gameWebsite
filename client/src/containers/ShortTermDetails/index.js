47e0bbac0fa8db7801977a417a88f9437bb33ea0
gcmartin <gscalica@gmail.com>
2020-11-24 12:21:53 -0800

    Create deleteShortTermGame backend API route and controller, and revise shortTermDetails component to redirect

--- client/src/containers/ShortTermDetails/index.js
+++ client/src/containers/ShortTermDetails/index.js
@@ -6,7 +6,8 @@ import './styles.css';
 
 class ShortTermDetails extends Component {
   state = {
-    gameData: ""
+    gameData: "",
+    redirect: false,
   }
 
   // for componentDidMount():
@@ -33,29 +34,52 @@ class ShortTermDetails extends Component {
     console.log(this.state.gameData, "line 33");
   }
 
+  // function for the child component to get the new route so the parent can redirect
+  redirect = (childData) => {
+    this.setState({ redirect: childData });
+  }
+
   render() {
     // get info from gameData state
     const { _id, description, drunkRulesLink, title, players, realRulesLink, src, time } = this.state.gameData;
 
     return (
-      <div className="ShortTermDetails-div">
-        {/* <img src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1314&q=80" /> */}
-        <p>Name: {title}</p>
-        <p>Description: {description}</p>
-        <p>Players: {players}</p>
-        <p>Time: {time}</p>
-        {/* <p>{drunkRulesLink}</p> */}
-        {/* <p>{realRulesLink}</p> */}
-        {/* <p>{src}</p> */}
 
-        <Button variant="warning">Real Rules</Button>
-        <Button variant="danger">Drunk Rules</Button>
-        <EditModal 
-          id={_id}
-          apiRoute="/api/shorttermgames/"
-          passDataToParent={this.passDataToParent}
-        />
+      <div>
+        {
+          // if this.state.redirect is "true"...
+          this.state.redirect
+          ?
+          // ...then go to "/ShortTermGames/" route...
+          this.props.history.push("/ShortTermGames")
+          :
+          // otherwise, display the game info
+          <div className="ShortTermDetails-div">
+            
+            <p>Name: {title}</p>
+            <p>Description: {description}</p>
+            <p>Players: {players}</p>
+            <p>Time: {time}</p>
+            {/* <p>{drunkRulesLink}</p> */}
+            {/* <p>{realRulesLink}</p> */}
+            {/* <p>{src}</p> */}
+
+            <Button variant="warning">Real Rules</Button>
+            <Button variant="danger">Drunk Rules</Button>
+            <EditModal 
+              id={_id}
+              passDataToParent={this.passDataToParent}
+              redirect={this.redirect}
+              apiRoute="/api/shorttermgames/"
+            />
+          </div>
+        }
       </div>
+
+
+
+
+      
     );
   }
 }
