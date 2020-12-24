22e1f5d7db6a992c17a40b5c421ce6c48e39e041
Jimmy Ngo <jngo2013@gmail.com>
2020-12-24 11:39:57 -0800

    fix logo in footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import './styles.css';
+import sample2 from './images/sample2.jpg'
 
 function Footer() {
   return (
@@ -14,7 +15,7 @@ function Footer() {
           <Col className="Footer-col" lg={4} md={6} sm={12}>
             <Link to="/">
               <Image
-                src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-0/p370x247/67529268_665627333913015_2337246147929702400_n.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=kSPsdRngwLcAX9UJVsW&_nc_ht=scontent-sjc3-1.xx&tp=6&oh=af476855dd55ba3c2227e2b324cb64cc&oe=5FA54906"
+                src={sample2}
                 className="Footer-logo"
                 roundedCircle
               />
