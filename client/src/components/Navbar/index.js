df2ec58844e6583a90045c11b9ec30490739a0f0
gcmartin <gscalica@gmail.com>
2020-12-24 11:25:56 -0800

    Move 'Add a Legacy Game' button to 'Legacy Games' page and style

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import { NavDropdown, Nav, Form, Navbar, Image } from 'react-bootstrap';
-import AddLegacyModal from './../../containers/AddLegacyModal';
 import AddShortModal from './../../containers/AddShortModal';
 import "./style.css";
 import SearchBar from "../SearchBar/index"
 import sample2 from "./images/sample2.jpg"
 
-
-
 function NavBar(props) {
   console.log(props.authenticated)
   return (
   // export default (props) => (
-<Navbar  className="NavbarBG" expand="lg">
-  <Navbar.Toggle  aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/"><Image   src={sample2} className="navbar-brand" roundedCircle></Image></Nav.Link>
-      <Nav.Link href="AboutUs" className="navPadding" >About Us</Nav.Link>
-      <NavDropdown title="Games" id="basic-nav-dropdown"  className="navPadding">
-        <NavDropdown.Item href="/LegacyGames">Legacy</NavDropdown.Item>
-        <NavDropdown.Item href="/ShortTermGames">ShortGames</NavDropdown.Item>
-      </NavDropdown>
-      { props.authenticated ? <Nav.Link href="/SignOut" className='navPadding'>Sign Out</Nav.Link> : null}
-    </Nav>
-    <Form inline>
-      <SearchBar />
-    </Form>
-    <AddLegacyModal />
-    <AddShortModal />
-  </Navbar.Collapse>
-</Navbar>
+    <Navbar  className="NavbarBG" expand="lg">
+      <Navbar.Toggle  aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/"><Image   src={sample2} className="navbar-brand" roundedCircle></Image></Nav.Link>
+          <Nav.Link href="AboutUs" className="navPadding" >About Us</Nav.Link>
+          <NavDropdown title="Games" id="basic-nav-dropdown"  className="navPadding">
+            <NavDropdown.Item href="/LegacyGames">Legacy</NavDropdown.Item>
+            <NavDropdown.Item href="/ShortTermGames">ShortGames</NavDropdown.Item>
+          </NavDropdown>
+          { props.authenticated ? <Nav.Link href="/SignOut" className='navPadding'>Sign Out</Nav.Link> : null}
+        </Nav>
+        <Form inline>
+          <SearchBar />
+        </Form>
+        <AddShortModal />
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
