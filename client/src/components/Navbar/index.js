9b23e7ffd2d59a064e9a1898319a4fcae9ada3f1
gcmartin <gscalica@gmail.com>
2021-01-04 15:59:51 -0800

    Fix Navbar styles and add media queries for hamburger menu

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,25 +7,45 @@ import sample2 from "./images/sample2.jpg"
 function NavBar(props) {
   console.log(props.authenticated)
   return (
-  // export default (props) => (
-    <Navbar  className="NavbarBG" expand="lg">
-      <Navbar.Toggle  aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/"><Image   src={sample2} className="navbar-brand" roundedCircle></Image></Nav.Link>
-          <Nav.Link href="AboutUs" className="navPadding" >About Us</Nav.Link>
-          <NavDropdown title="Games" id="basic-nav-dropdown"  className="navPadding">
-            <NavDropdown.Item href="/LegacyGames">Legacy</NavDropdown.Item>
-            <NavDropdown.Item href="/ShortTermGames">Board Games</NavDropdown.Item>
-          </NavDropdown>
-          { props.authenticated ? <Nav.Link href="/SignOut" className='navPadding'>Sign Out</Nav.Link> : null}
-        </Nav>
-        <Form inline>
-          <SearchBar />
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
+      <Navbar bg="dark" expand="lg" className="NavbarBG">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/"><Image src={sample2} className="navbar-brand" roundedCircle></Image></Nav.Link>
+            <Nav.Link href="/AboutUs">About Us</Nav.Link>
+            <NavDropdown title="Games" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/LegacyGames">Legacy</NavDropdown.Item>
+              <NavDropdown.Item href="/ShortTermGames">Board Games</NavDropdown.Item>
+            </NavDropdown>
+            { props.authenticated ? <Nav.Link href="/SignOut" className='navPadding'>Sign Out</Nav.Link> : null}
+          </Nav>
+          <Form inline>
+            <SearchBar />
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
   );
 }
 
 export default NavBar;
+
+
+// export default (props) => (
+      // ===== ORIGINAL NAVBAR ===== 
+      // <Navbar bg="dark"  className="NavbarBG" expand="lg">
+      //   <Navbar.Toggle  aria-controls="basic-navbar-nav" />
+      //   <Navbar.Collapse id="basic-navbar-nav">
+      //     <Nav className="mr-auto">
+      //       <Nav.Link href="/"><Image   src={sample2} className="navbar-brand" roundedCircle></Image></Nav.Link>
+      //       <Nav.Link href="AboutUs" className="navPadding" >About Us</Nav.Link>
+      //       <NavDropdown title="Games" id="basic-nav-dropdown"  className="navPadding">
+      //         <NavDropdown.Item href="/LegacyGames">Legacy</NavDropdown.Item>
+      //         <NavDropdown.Item href="/ShortTermGames">Board Games</NavDropdown.Item>
+      //       </NavDropdown>
+      //       { props.authenticated ? <Nav.Link href="/SignOut" className='navPadding'>Sign Out</Nav.Link> : null}
+      //     </Nav>
+      //     <Form inline>
+      //       <SearchBar />
+      //     </Form>
+      //   </Navbar.Collapse>
+      // </Navbar>
