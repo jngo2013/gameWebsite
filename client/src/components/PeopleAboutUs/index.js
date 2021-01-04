742086e656d9f2a338082567575fc4435ccb164b
Jimmy Ngo <jngo2013@gmail.com>
2021-01-04 14:39:54 -0800

    reorganize names to fit under picture

--- client/src/components/PeopleAboutUs/index.js
+++ client/src/components/PeopleAboutUs/index.js
@@ -24,14 +24,16 @@ function People(){
       src={JG}
       roundedCircle
       
-    /></Col>
+    /><h1>Jonathan Grothe</h1>
+    </Col>
+
     <Col className="textCenter">
     Jonathan is a long running board game enthusiast. Founder of Drunk on Games when taking his top two passions and mixing them together. You'll find him at his local board game club SJ Gamekrackers making sure everyone learns some new games and has a good time. Lives by the motto "Everyone loves board games, most just don't know it yet."
     </Col>
       </Row>
     
     </Container>
-    <h1>Jonathan Grothe</h1>
+    
     <hr></hr>
     <br></br>
     
@@ -42,14 +44,16 @@ function People(){
       src={AL}
       roundedCircle
       
-    /></Col>
+    /><h1>Albert Lee</h1>
+    </Col>
+
     <Col className="textCenter">
     <div>The local rules lawyer, co-op advocate, and camera coordinator, I am constantly looking to optimize in both games and recording content. When a specific issue comes up, I'm either asking about it, or...using it to my advantage.</div>
     </Col>
       </Row>
     
     </Container>
-    <h1>Albert Lee</h1>
+    
     <hr></hr>
     <br></br>
 
@@ -60,14 +64,16 @@ function People(){
       src={GG}
       roundedCircle
       
-    /></Col>
+    /><h1>Gabe Golden</h1>
+    </Col>
+
     <Col className="textCenter">
     <div>Golden is a Linguist who loves languages, voice acting, and of course boardgames! His favorite types of games are push your luck,  social deduction, and party games.  He is currently creating Boardgames that help people learn languages. You can follow him on Instagram as GoldenFedoraFox or his projects under Passionfruit Education on all social media sites or <a href="https://www.passionfruiteducation.com" target="_blank" rel="noopener noreferrer">PassionfruitEducation.com</a></div>
     </Col>
       </Row>
     
     </Container>
-    <h1>Gabe Golden</h1>
+    
     <hr></hr>
     <br></br>
 
@@ -78,18 +84,21 @@ function People(){
       src={May}
       roundedCircle
       
-    /></Col>
+    /><h1>Maybeline</h1>
+    </Col>
+
     <Col className="textCenter">
-    <div>Hello! Maybeline here, at your service!
+    <div><p>Hello! Maybeline here, at your service!</p>
 
-I love the board game world! There is something wonderful about sitting around a table with people and a game in front of us that brings closer connections with one other. An extra bonus is when there are drinking rules (alcoholic beverages and non-alcoholic beverages). My strategy when playing a strategic game (whether it is co-op or not) is to be in my own world and analyze other player's moves. I'll be that "quiet player" that does their own thing and everyone forgets about and ends up winning the game. It works like a charm (most of the time).
+<p>I love the board game world! There is something wonderful about sitting around a table with people and a game in front of us that brings closer connections with one other. An extra bonus is when there are drinking rules (alcoholic beverages and non-alcoholic beverages). My strategy when playing a strategic game (whether it is co-op or not) is to be in my own world and analyze other player's moves. I'll be that "quiet player" that does their own thing and everyone forgets about and ends up winning the game. It works like a charm (most of the time).</p>
 
-I spend most of my time behind the scene for Drunk on Games as a video editor providing great content for the viewers. So if you see me in a video, comment on that video a cherry blossom flower "🌸" emoji!</div>
+<p>I spend most of my time behind the scene for Drunk on Games as a video editor providing great content for the viewers. So if you see me in a video, comment on that video a cherry blossom flower "🌸" emoji!</p>
+</div>
     </Col>
       </Row>
     
     </Container>
-    <h1>Maybeline</h1>
+    
     <hr></hr>
     <br></br>
     </div>
