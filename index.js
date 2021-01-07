6995b54654be97e41255d8749bb74ea25b906309
Jimmy Ngo <jngo2013@gmail.com>
2021-01-06 19:01:14 -0800

    add path for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +13,9 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use(routes);
