83a0d7b2ddf2c8b0da4245ac53bf826026c3b6f4
Jimmy Ngo <jngo2013@gmail.com>
2021-01-08 14:52:33 -0800

    testing heroku build app

--- index.js
+++ index.js
@@ -10,22 +10,20 @@ const app = express();
 // Setup middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.use(routes);
 require('./services/passport');
 // Connect database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gamewebsite',
-//mongoose.connect("mongodb+srv://User21:K973Mjs7DJPLVDU@dogweb.oqxs2.mongodb.net/gamewebsite?retryWrites=true&w=majority",
  { useNewUrlParser: true,
    useCreateIndex: true,
    useUnifiedTopology: true,
    useFindAndModify: false });
+
+   if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(PORT);
