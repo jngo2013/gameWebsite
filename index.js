8ad40b7a265af2176dd64789cfa313b924067bd0
gcmartin <gscalica@gmail.com>
2021-01-07 17:31:54 -0800

    add '../' to express.static

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
