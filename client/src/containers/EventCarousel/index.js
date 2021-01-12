3d77abc0bd291419d242c7b71da34081096fd0c3
gcmartin <gscalica@gmail.com>
2021-01-12 12:08:09 -0800

    Move componentDidMount from EventCarousel to Home and pass props

--- client/src/containers/EventCarousel/index.js
+++ client/src/containers/EventCarousel/index.js
@@ -12,28 +12,7 @@ import './style.css';
 class ControlledCarousel extends Component {
   state = {
     index: 0,
-    // carouselData: {
-    //   slide1: "4",
-    //   slide1desc: "Will Chet ever manage to kill a single Stone Golem?",
-    //   slide2: "3",
-    //   slide2desc: "What are the zealots up to?",
-    //   slide3: "1",
-    //   slide3desc: "The start of our journey to the Jaws of the Lion!",
-    // }
-    carouselData: ""
   }
-
-  async componentDidMount() {
-    try {
-      let response = await axios.get("/api/eventcarousel");
-      // console.log(response.data, "line 28");
-      this.setState({carouselData: response.data});
-      console.log(this.state.carouselData);
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
 
   handleSelect = (selectedIndex, event) => {
     this.setState({ index: selectedIndex });
@@ -52,50 +31,48 @@ class ControlledCarousel extends Component {
         <Carousel className="Carousel" activeIndex={this.state.index} onSelect={this.handleSelect}>
           {/* Slide 1 */}
           <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={jol4}
-                alt="First slide"
-              />
-              
-              {/* <Carousel.Caption>
-                <p>{this.state.carouselData.slide1}</p>
-                <h3>Episode {this.state.carouselData.slide1}</h3>
-                <p>{this.state.carouselData.slide1desc}</p>
-              </Carousel.Caption> */}
-            </Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={jol4}
+              alt="First slide"
+            />
+          
+            <Carousel.Caption>
+              <h3>Episode {this.props.carouselData.slide1}</h3>
+              <p>{this.props.carouselData.slide1desc}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-            {/* Slide 2 */}
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={jol3}
-                alt="Second slide"
-              />
-              {/* <Carousel.Caption>
-                <h3>Episode {this.state.carouselData.slide2}</h3>
-                <p>{this.state.carouselData.slide2desc}</p>
-              </Carousel.Caption> */}
-            </Carousel.Item>
+          {/* Slide 2 */}
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={jol3}
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Episode {this.props.carouselData.slide2}</h3>
+              <p>{this.props.carouselData.slide2desc}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-            {/* Slide 3 */}
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={jol1}
-                alt="Third slide"
-              />
-              {/* <Carousel.Caption>
-                <h3>Episode {this.state.carouselData.slide3}</h3>
-                <p>{this.state.carouselData.slide3desc}</p>
-              </Carousel.Caption> */}
-            </Carousel.Item>
+          {/* Slide 3 */}
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={jol1}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Episode {this.props.carouselData.slide3}</h3>
+              <p>{this.props.carouselData.slide3desc}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
           </Carousel>
     
         {/* <EditCarouselModal passDataToParent={this.passDataToParent} carouselData={this.state.carouselData}/> */}
 
-        
       </Container>
     );
   }
--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import CardSection from "../CardSection";
+import axios from 'axios';
 import ControlledCarousel from "../EventCarousel";
 import "./styles.css"
 
 class Home extends Component {
+  state = {
+    carouselData: "",
+  }
+
+  // get data from the database for the carousel
+  async componentDidMount() {
+    try {
+      let response = await axios.get("/api/eventcarousel");
+      // convert response.data into an array
+      let carouselDataArr = Object.entries(response.data)[0][1];
+      this.setState({carouselData: carouselDataArr});
+    } catch (err) {
+      console.log(err)
+    }
+  }
   
   render(){
     return(
@@ -12,12 +28,11 @@ class Home extends Component {
         <h1 className="H1Text">Welcome</h1>
         
         <hr></hr>
-        <ControlledCarousel />
+        <ControlledCarousel carouselData={this.state.carouselData}/>
         <CardSection />
       </div>
-      
     )
   }
-  }
+}
 
-  export default Home
+export default Home
