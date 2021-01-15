b829fdb15bd22d331d085b8b5163a5f466be937b
gcmartin <gscalica@gmail.com>
2021-01-15 15:49:03 -0800

    Add margin to 'Search' button

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
+import './styles.css';
 
 class SearchBox extends Component {
   state = {
@@ -9,28 +10,28 @@ class SearchBox extends Component {
   handleInputChange = events => {
     this.setState({searchInput:events.target.value})
   }
+
   handleSubmit = events => {
     events.preventDefault();
     alert(this.state.searchInput)
   }
-  render()
-  {
+
+  render() {
     return (
-    <div className="searchbox">
-      <form className="form-inline" onSubmit={this.handleSubmit}>
-        <input
-          className="form-control"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-          onChange={this.handleInputChange}
-        />
-        
-        <Button variant="outline-warning" type="submit">Search</Button>
-      </form>
-    </div>
-  );
-}
-  
+      <div className="searchbox">
+        <form className="form-inline" onSubmit={this.handleSubmit}>
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={this.handleInputChange}
+          />
+          
+          <Button variant="outline-warning" type="submit" className="search-btn"><span>Search</span></Button>
+        </form>
+      </div>
+    );
+  } 
 }
 export default SearchBox;
