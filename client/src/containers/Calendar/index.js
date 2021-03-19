1b2893f0c28ed1f079fc6e076944b39a8483aed1
gcmartin <gscalica@gmail.com>
2021-03-19 11:56:09 -0700

    Add CSS for calendar and style

--- client/src/containers/Calendar/index.js
+++ client/src/containers/Calendar/index.js
@@ -4,7 +4,8 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import { INITIAL_EVENTS, createEventId } from './eventDb'
-import axios from 'axios'
+import axios from 'axios';
+import './styles.css';
 
 
 export default class EventCalendar extends React.Component {
@@ -26,38 +27,9 @@ export default class EventCalendar extends React.Component {
   render() {
     return (
       <div className='demo-app'>
-        {this.renderSidebar()}
-        <div className='demo-app-main'>
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            initialView='dayGridMonth'
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={this.state.weekendsVisible}
-            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-            // select={this.handleDateSelect}
-            select={this.state.authenticated ? this.handleDateSelect : null} // if user is authenticated, allow to add an event
-            eventContent={renderEventContent} // custom render function
-            eventClick={this.handleEventClick}
-            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            /* you can update a remote database when these fire:
-            
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            */
-           
-          />
-        </div>
+        
 
         {/* ===== ADD GOOGLE CALENDAR CODE HERE ===== */}
-        <iframe src="" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
     )
   }
@@ -160,3 +132,35 @@ function renderSidebarEvent(event) {
     </li>
   )
 }
+
+
+// old calendar
+// {this.renderSidebar()}
+//         <div className='demo-app-main'>
+//           <FullCalendar
+//             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+//             headerToolbar={{
+//               left: 'prev,next today',
+//               center: 'title',
+//               right: 'dayGridMonth,timeGridWeek,timeGridDay'
+//             }}
+//             initialView='dayGridMonth'
+//             editable={true}
+//             selectable={true}
+//             selectMirror={true}
+//             dayMaxEvents={true}
+//             weekends={this.state.weekendsVisible}
+//             initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+//             // select={this.handleDateSelect}
+//             select={this.state.authenticated ? this.handleDateSelect : null} // if user is authenticated, allow to add an event
+//             eventContent={renderEventContent} // custom render function
+//             eventClick={this.handleEventClick}
+//             eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+//             /* you can update a remote database when these fire:
+            
+//             eventChange={function(){}}
+//             eventRemove={function(){}}
+//             */
+           
+//           />
+//         </div>
