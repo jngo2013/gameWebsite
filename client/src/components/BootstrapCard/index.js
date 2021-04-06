ff538e41b708f721ebf3db3595c4b2c7be7efdda
gcmartin <gscalica@gmail.com>
2021-04-06 11:28:10 -0700

    Rename BootstrapCard.css to styles.css and clean up code

--- client/src/components/BootstrapCard/index.js
+++ client/src/components/BootstrapCard/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-import './BootstrapCard.css';
+import './styles.css';
 
 function BootstrapCard(props) {
   return (
-    // <div>
-    
-      // <Card style={{ width: '30rem' }} className="BootstrapCard">
-      <div className='BootstrapCard-div'>
-        <Card className="BootstrapCard">
-          <Link to={`/${props.page}`}>
-            <Card.Img variant="top" src={props.src} />
-            <h2 className="BootstrapCard-title">{props.title}</h2>
-            <h3 className="BootstrapCard-description">{props.description}</h3>
-          </Link>
-        </Card>
-      </div>
-
-    // </div>
+    <div className='BootstrapCard-div'>
+      <Card className="BootstrapCard">
+        <Link to={`/${props.page}`}>
+          <Card.Img variant="top" src={props.src} />
+          <h2 className="BootstrapCard-title">{props.title}</h2>
+          <h3 className="BootstrapCard-description">{props.description}</h3>
+        </Link>
+      </Card>
+    </div>
   );
 }
 
--- client/src/components/KitchenSinkCard/index.js
+++ client/src/components/KitchenSinkCard/index.js
@@ -5,28 +5,19 @@ import './styles.css';
 function KitchenSinkCard(props) {
   return (
     <div className="KitchenSinkCard-div">
-      {/* <Card style={{ width: '21rem', height: '37rem' }} bg="secondary" className="KitchenSinkCard"> */}
       <Card bg="secondary" className="KitchenSinkCard">
-
+        {/* Card image */}
         <div className="KitchenSinkCard-img-div">
           <Card.Img 
               variant="top" 
-              // if 'props.src' is undefined, show the replacement image, otherwise show props.src
-              // src={ props.src === undefined 
-              //     ? "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=859&q=80" 
-              //     : props.src } 
               src={props.src}
               className="KitchenSinkCard-img" 
           />
         </div>
           
-        
-        
-
-        {/* <Card.Body className="overflow-auto"> */}
+        {/* Card body */}
         <Card.Body className="KitchenSinkCard-body">
           <Card.Title>{props.name}</Card.Title>
-          {/* <Card.Text >{props.description}</Card.Text> */}
         </Card.Body>
 
         <ListGroup className="list-group-flush">
@@ -38,7 +29,6 @@ function KitchenSinkCard(props) {
             <span><i class="fas fa-beer"></i> {props.drunkRating}%</span>
           </ListGroupItem>
         </ListGroup>
-
       </Card>
     </div>
   );
