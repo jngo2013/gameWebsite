e9fa6c74cebfc23f730f80cbd1f2871d4af973be
Jimmy Ngo <jngo2013@gmail.com>
2021-04-06 23:06:24 -0700

    delete calendarRoutes. Unused

--- routes/apiRoutes/calendarRoutes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {addCalEvent,getAllCalEvents} = require("../../../controllers/calendarController")
-
-router.post("/", addCalEvent)
-router.get("/", getAllCalEvents)
-// router.post('/', () => console.log('this is the calendar route'))
-
-module.exports = router;
