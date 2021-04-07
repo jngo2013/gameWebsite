ef4ec513f900cee542bb243f7fcacc9f14d2cf54
Jimmy Ngo <jngo2013@gmail.com>
2021-04-07 00:46:00 -0700

    remove calendar from models

--- models/index.js
+++ models/index.js
@@ -2,12 +2,10 @@ const Legacy = require('./LegacyGame');
 const Short = require('./ShortTermGame');
 const User = require('./User');
 const EventCarousel = require('./EventCarousel');
-const Calendar = require('./Calendar')
 
 module.exports = {
   Legacy,
   Short,
   User,
   EventCarousel,
-  Calendar,
 };
